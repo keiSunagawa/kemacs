601ecb5d1c2965ad7640ab4dc9ade73e7e989cb7
keiSunagawa <kei.sunagawa@mvrck.co.jp>
2018-12-30 11:27:00 +0900

    darwin setting

---
+++ .emacs.d/init/01_darwin_setting.el
@@ -0,0 +1,18 @@
+(when (eq system-type 'darwin)
+  (progn
+    (defun copy-from-osx ()
+      (shell-command-to-string "pbpaste"))
+
+    (defun paste-to-osx (text &optional push)
+      (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+          (process-send-string proc text)
+          (process-send-eof proc))))
+
+    (setq interprogram-cut-function 'paste-to-osx)
+    (setq interprogram-paste-function 'copy-from-osx)
+
+    (add-to-list 'load-path "~/.emacs.d/lib/site-lisp")
+    (require 'eaw)
+    (eaw-fullwidth)))
+
--- .emacs.d/init/10_global_settings.el
+++ .emacs.d/init/10_global_settings.el
@@ -40,21 +40,6 @@
                (setq eldoc-echo-area-use-multiline-p t)
                (turn-on-eldoc-mode))))
 
-;; copy sync clipbord
-(if (eq system-type 'darwin)
-    (progn
-      (defun copy-from-osx ()
-        (shell-command-to-string "pbpaste"))
-
-      (defun paste-to-osx (text &optional push)
-        (let ((process-connection-type nil))
-          (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-            (process-send-string proc text)
-            (process-send-eof proc))))
-
-      (setq interprogram-cut-function 'paste-to-osx)
-      (setq interprogram-paste-function 'copy-from-osx)))
-
 ;; auto insert pair char
 (electric-pair-mode 1)
 
