005fc0ba7f0a747717ef2d58e16c74b79790a0c8
keiSunagawa <keisunagawa.git@icloud.com>
2019-09-17 01:46:36 +0900

    protect func 3

--- .emacs.d/init/k-util/protect.el
+++ .emacs.d/init/k-util/protect.el
@@ -9,6 +9,9 @@
 
 (defmacro F (&rest xs) `(k/forgelin ,xs))
 
+;; A => F[A]
+(defun k/pure (a) (list 'k/succ a))
+
 ;; F[A] => (A => F[B]) => F[B]
 (defun k/bind (adt f)
   (pcase adt
@@ -22,8 +25,13 @@
     (`(k/fail ,ev) (list 'k/fail ev))))
 
 
-;;
-
+;;----------------------------
+;;(k/do (
+;;        (a <- (F + 1 5))
+;;        (b <- (F + a 2))
+;;        (pure (+ a b))
+;;       ))
+;;------------------------------
 ;; (defmacro k/do (bodies)
 ;;   (car (_k/do-f (-map (lambda (b) `,b) bodies))))
 (defmacro k/do (bodies) `(_k/do-f ',bodies))
@@ -45,6 +53,8 @@
 (defun _k/do-parse-phase (env body)
   (pcase body
     (`(,sym <- ,f) (_k/do-eval-bind sym f env))
+    (`(pure ,f) (_k/do-eval-pure f env))
+    (`,f (_k/do-eval-row f env))
     (`,n (error (format "unmatch syntax %s" n)))
     ))
 
@@ -52,12 +62,30 @@
 (defun _k/do-eval-bind (sym f env)
   (progn
     (message (format "%s" f))
-    (k/map (eval f) (lambda (res)
+    (k/map (eval (_k/replace-var-list f env)) (lambda (res)
                        (let* ((new-env (cons (list sym res) env)))
                        (cons res new-env))))))
 
 ;; (defun _k/do-parse-let () 1)
-(defun _k/do-eval-pure () 1)
-(defun _k/do-eval-row () 1)
+(defun _k/do-eval-row (f env) (k/map (eval (_k/replace-var-list f env)) (lambda (rf) (cons rf env))))
+(defun _k/do-eval-pure (f env) (k/map (k/pure (eval (_k/replace-var-list f env))) (lambda (rf) (cons rf env))))
+
+(defun _k/replace-var-list (f env) (-map (lambda (v) (_k/replace-var v env)) f))
+
+(defun _k/replace-var (v env)
+  (cond
+       ((symbolp v) (_k/do-replace v env))
+       ((listp v) (_k/replace-var-list v env))
+       (t v)
+  ))
+
+(defun _k/do-replace (v env)
+  (progn
+    (message (format "env is %s" env))
+    (message (format "key is %s" v))
+    (message (format "lookup is %s" (assoc v env)))
+    (let* ((lookv (cadr (assoc v env))))
+    (if (null lookv) v lookv)
+    )))
 
 (provide 'protect)
